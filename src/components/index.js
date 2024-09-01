5b9129caf5b62bb7a8c16e4162730a6115e174ee
Karthiga-l <karthiganathan.93@gmail.com>
2024-09-01 09:55:30 +0530

    First inital code complete

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { Header } from './Header';
+export { Footer } from './Footer';
+
+export { MovieCard } from './MovieCard';
+export { Button } from './Button';
+
+export { ScrollToTopFirst } from './ScrollToTopFirst';
---
+++ src/pages/PageNotFound.js
@@ -0,0 +1,32 @@
+import PageNotFoundImage from '../assets/images/pagenotfound.png';
+import { Link } from 'react-router-dom';
+import { Button } from '../components';
+import { useTitle } from '../hooks';
+
+export const PageNotFound = () => {
+  useTitle(`Page Not Found / Cinemate`);
+
+  return (
+    <main>
+      <section className="flex flex-col justify-center px-2">
+        <div className="flex flex-col items-center my-4">
+          <p className="text-7xl text-gray-700 font-bold my-10 dark:text-white">
+            404, Oops!
+          </p>
+          <div className="max-w-lg">
+            <img
+              className="rounded"
+              src={PageNotFoundImage}
+              alt="404 Page Not Found"
+            />
+          </div>
+        </div>
+        <div className="flex justify-center my-4">
+          <Link to="/">
+            <Button children="Back to Cinemate" />
+          </Link>
+        </div>
+      </section>
+    </main>
+  );
+};
